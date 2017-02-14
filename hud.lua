fc6dbcd69146cd0ed2016477fee4bfb30ebc3d07
karonka <karonka@abv.bg>
2017-02-14 22:12:40 +0200

    ..

--- hud.lua
+++ hud.lua
@@ -65,8 +65,26 @@ function drawGameOverScreen()
 end
 
 function drawInventory()
-	local x,y = 500,20
+	local x,y = 400,20
 	local size = SCREEN_WIDTH/24
+	local scale = 1
+	for i = 1,2 do
+		local weapon = PLAYER.children[i]
+		if weapon.active then
+			love.graphics.setColor(153,0,102)
+			love.graphics.rectangle( 'line', x, y, size, size, 3, 3)
+		else
+			love.graphics.rectangle( 'line', x, y, size, size, 3, 3)
+			love.graphics.print( {{0,0,0,120},'q'}, x + 0.8*size, y + 0.8*size)
+		end
+		love.graphics.setColor(255,255,255,255)
+		local quad = Images[weapon.weaponType][weapon.state][1] or Images[weapon.weaponType][weapon.state]-- or Images[item.itemType]['normal']
+		_,_,w,h = quad:getViewport()
+		scale = size*0.8/(w > h and w or h)
+		love.graphics.draw(Images[weapon.weaponType]["sprite"], quad, x + 3 + size/2 - w*scale/2, y + 3 + size/2 - h*scale/2, 0, scale)
+		x = x + size + 3
+	end
+	x = x + size/3
 	for i = 1,9 do
 		love.graphics.rectangle( 'line', x, y, size, size, 3, 3)
 		love.graphics.print( {{0,0,0,120},tostring(i)}, x + 0.8*size, y + 0.8*size)
@@ -74,11 +92,11 @@ function drawInventory()
 			local item = PLAYER.inventory[i]
 			local quad = Images[item.itemType][item.state][1] or Images[item.itemType][item.state]-- or Images[item.itemType]['normal']
 			_,_,w,h = quad:getViewport()
-			love.graphics.draw(Images[item.itemType]["sprite"], quad, x + 3, y + 3, 0, size*0.8/w)
+			scale = size*0.8/(w > h and w or h)
+			love.graphics.draw(Images[item.itemType]["sprite"], quad, x + 3, y + 3, 0, scale)
 		end
 		x = x + size + 3
 	end
-	
 end
 
 
