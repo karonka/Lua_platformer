cc359518b29e5f34ed0b8288deebbb34430823ac
samir-github <Samir>
2017-02-14 18:46:48 +0200

    Fixed visual bug on some resolutions

--- camera.lua
+++ camera.lua
@@ -2,8 +2,9 @@ Camera = {
   target = nil,
   tX = 0,
   tY = 0,
-  scaleX = SCREEN_WIDTH /(1920 * PERCENT_OF_SCREEN_FILLED),
+  scaleX = math.floor(SCREEN_WIDTH /(1920 * PERCENT_OF_SCREEN_FILLED) * 35) / 35,
   scaleY = SCREEN_HEIGHT / (1080 * PERCENT_OF_SCREEN_FILLED) 
+     
 }
 
 
@@ -14,6 +15,7 @@ function Camera:setTarget(_target)
 end
 
 function Camera.update(dt)
+    print(Camera.scaleX)
     Camera.tX = lerp(Camera.tX,SCREEN_WIDTH/2 - Camera.target.x * Camera.scaleX, math.min(1, 3*dt)) 
     Camera.tY = lerp(Camera.tY,SCREEN_HEIGHT/2 - Camera.target.y * Camera.scaleY, math.min(1, 3*dt))
     Camera.tX = clamp(-WORLD_WIDTH * Camera.scaleX + SCREEN_WIDTH, Camera.tX, 0)
